1689f7968bb2a8ac9ec1b2e365ccb02529ae6bf4
Kurt Vooys <kurt.vooys@gmail.com>
2022-09-11 08:16:51 +0200

    Fix about/contact pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,19 +5,8 @@
 
 <h1>Contact</h1>
 
-This site was made by Kurt Vooys. You can get in touch with me here.
+<p>
+This site was made by Kurt Vooys. 
 
-<form name="take-the-tempo-contact-form" method="POST" data-netlify="true">
-	<p>
-	  <label>Your Name: <input type="text" name="name" /></label>
-	</p>
-	<p>
-	  <label>Your Email: <input type="email" name="email" /></label>
-	</p>
-	<p>
-	  <label>Message: <textarea name="message" placeholder="Your message here..."></textarea></label>
-	</p>
-	<p>
-	  <button type="submit">Send</button>
-	</p>
-  </form>
+You can get in touch with me through <a href="mailto:kurt.vooys@gmail.com">e-mail.</a>
+</p>

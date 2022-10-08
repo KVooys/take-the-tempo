d3951079a06d9afc6e167f8a150d6e0f1b8d99ec
Kurt Vooys <kurt.vooys@gmail.com>
2022-10-08 10:45:22 +0200

    Enable latest post route

--- src/routes/blog/[post]/+page.server.js
+++ src/routes/blog/[post]/+page.server.js
@@ -1,7 +1,15 @@
-import { error } from '@sveltejs/kit'
+import { error, redirect } from '@sveltejs/kit'
+import fetchPosts from '$lib/assets/js/fetchPosts'
 
 export const load = async ({ params }) => {
-	try {	
+	try {
+		
+		if (params.post == "latest") {
+			const { posts } = await fetchPosts();
+			const latestPost = posts[0].slug;
+			params.post = latestPost;
+		}
+
 		const post = await import(`../../../lib/posts/${params.post}.md`)
 
 		return {
